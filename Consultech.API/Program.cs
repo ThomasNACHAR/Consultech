49f96eeead83cbe2362a58400fa6d973dc77664f
pierre delarocque <delarocque.pierre@gmail.com>
2025-10-15 14:45:22 +0200

    add builder at program.cs/Consultech.API

--- Consultech.API/Program.cs
+++ Consultech.API/Program.cs
@@ -1,3 +1,6 @@
+
+using Consultech.Business.Extensions;
+
 using Consultech.DAL;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,6 +16,9 @@ builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<ConsultechDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddConsultechBusiness();
+
 
 var app = builder.Build();
 
