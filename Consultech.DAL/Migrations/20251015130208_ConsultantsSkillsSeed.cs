fbcd46a2076bbc2ecd9a5e7b2a536d3d61f13803
Thomas NACHAR <thomas.nachar18@gmail.com>
2025-10-15 15:03:15 +0200

    Seed Consultants Skills

---
+++ Consultech.DAL/Migrations/20251015130208_ConsultantsSkillsSeed.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Consultech.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConsultantsSkillsSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ConsultantSkill",
+                columns: new[] { "ConsultantsId", "SkillsId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 1, 4 },
+                    { 2, 2 },
+                    { 2, 5 },
+                    { 3, 1 },
+                    { 3, 3 },
+                    { 4, 3 },
+                    { 5, 4 },
+                    { 5, 5 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ConsultantSkill",
+                keyColumns: new[] { "ConsultantsId", "SkillsId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "ConsultantSkill",
+                keyColumns: new[] { "ConsultantsId", "SkillsId" },
+                keyValues: new object[] { 1, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "ConsultantSkill",
+                keyColumns: new[] { "ConsultantsId", "SkillsId" },
+                keyValues: new object[] { 2, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "ConsultantSkill",
+                keyColumns: new[] { "ConsultantsId", "SkillsId" },
+                keyValues: new object[] { 2, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "ConsultantSkill",
+                keyColumns: new[] { "ConsultantsId", "SkillsId" },
+                keyValues: new object[] { 3, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "ConsultantSkill",
+                keyColumns: new[] { "ConsultantsId", "SkillsId" },
+                keyValues: new object[] { 3, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "ConsultantSkill",
+                keyColumns: new[] { "ConsultantsId", "SkillsId" },
+                keyValues: new object[] { 4, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "ConsultantSkill",
+                keyColumns: new[] { "ConsultantsId", "SkillsId" },
+                keyValues: new object[] { 5, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "ConsultantSkill",
+                keyColumns: new[] { "ConsultantsId", "SkillsId" },
+                keyValues: new object[] { 5, 5 });
+        }
+    }
+}
